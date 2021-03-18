c1f34e2c36c47d7cf1ab036912cabcbc44d32635
Ruy Antonio Gurjão Junior <ruy.junior@corp.net>
2021-03-18 07:47:03 -0300

    Atualizando JSON

--- js/app.js
+++ js/app.js
@@ -9,4 +9,9 @@ fetch('https://raw.githubusercontent.com/gurjao/curriculum/master/info.json')
     document.querySelector('#nome').innerHTML = json.nome;
     document.querySelector('#cargo').innerHTML = json.cargo;
     document.querySelector('#sobre').innerHTML = json.sobre;
+    document.querySelector('#contatoEmail').innerHTML = json.contato.email;
+    document.querySelector('#contatoCel').innerHTML = json.contato.cel;
+    document.querySelector('#contatoCidade').innerHTML = json.contato.cidade;
+    document.querySelector('#contatoPerfilLinkedin').innerHTML = json.contato.perfil.linkedin;
+    document.querySelector('#contatoPerfilSkype').innerHTML = json.contato.email;
   })
