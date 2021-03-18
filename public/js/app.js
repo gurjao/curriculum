5b5c9410a1040a9dc7b157e526480c218aeac8fc
Ruy Antonio Gurjão Junior <ruy.junior@corp.net>
2021-03-18 13:42:25 -0300

    Atualizando JSON

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,7 @@ fetch('https://raw.githubusercontent.com/gurjao/curriculum/master/info.json')
     return response.json();
   })
   .then(function(json) {
+    /* Dados pessoais */
     document.querySelector('#nome').innerHTML = json.nome;
     document.querySelector('#cargo').innerHTML = json.cargo;
     document.querySelector('#sobre').innerHTML = json.sobre;
@@ -14,6 +15,8 @@ fetch('https://raw.githubusercontent.com/gurjao/curriculum/master/info.json')
     document.querySelector('#contatoCidade').innerHTML = json.contato.cidade;
     document.querySelector('#contatoPerfilLinkedin').innerHTML = json.contato.perfil.linkedin;
     document.querySelector('#contatoPerfilSkype').innerHTML = json.contato.perfil.skype;
+
+    /* Experiência profissional */
     document.querySelector('#experienciaEmpresa3').innerHTML = json.experiencia[0].empresa;
     document.querySelector('#experienciaPeriodo3').innerHTML = json.experiencia[0].periodo;
     document.querySelector('#experienciaCargo3').innerHTML = json.experiencia[0].cargo;
@@ -26,17 +29,40 @@ fetch('https://raw.githubusercontent.com/gurjao/curriculum/master/info.json')
     document.querySelector('#experienciaPeriodo1').innerHTML = json.experiencia[2].periodo;
     document.querySelector('#experienciaCargo1').innerHTML = json.experiencia[2].cargo;
     document.querySelector('#experienciaResumo1').innerHTML = json.experiencia[2].resumo;
+
+    /* Formação */
     document.querySelector('#formacaoCurso2').innerHTML = json.formacao[0].graduacao;
     document.querySelector('#formacaoPeriodo2').innerHTML = json.formacao[0].periodo;
     document.querySelector('#formacaoInstituicao2').innerHTML = json.formacao[0].instituicao;
     document.querySelector('#formacaoCurso1').innerHTML = json.formacao[1].graduacao;
     document.querySelector('#formacaoPeriodo1').innerHTML = json.formacao[1].periodo;
     document.querySelector('#formacaoInstituicao1').innerHTML = json.formacao[1].instituicao;
+    
+    /* Curso */
     document.querySelector('#cursoInstituicao').innerHTML = json.curso[0].instituicao;
     document.querySelector('#cursoNome4').innerHTML = json.curso[0].nome[0];
     document.querySelector('#cursoNome3').innerHTML = json.curso[0].nome[1];
     document.querySelector('#cursoNome2').innerHTML = json.curso[0].nome[2];
     document.querySelector('#cursoNome1').innerHTML = json.curso[0].nome[3];
+    
+    /* Habilidade */
+    document.querySelector('#habilidade1').innerHTML = json.habilidade[2].nome;
+    document.querySelector('#habilidade2').innerHTML = json.habilidade[1].nome;
+    document.querySelector('#habilidade3').innerHTML = json.habilidade[0].nome;
+    
+    /* Idioma */
+    document.querySelector('#idioma1').innerHTML = json.idioma[0].nome;
+    document.querySelector('#idioma2').innerHTML = json.idioma[1].nome;
+    
+    /* Personalidade */
+    document.querySelector('#personalidade1').innerHTML = json.personalidade[4];
+    document.querySelector('#personalidade2').innerHTML = json.personalidade[3];
+    document.querySelector('#personalidade3').innerHTML = json.personalidade[2];
+    document.querySelector('#personalidade4').innerHTML = json.personalidade[1];
+    document.querySelector('#personalidade5').innerHTML = json.personalidade[0];
+    
+    /* Agradecimento */
+    document.querySelector('#agradecimento').innerHTML = json.agradecimento;
 })
 
 /*
